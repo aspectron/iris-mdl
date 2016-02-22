29c4969833f00252f3887ace590e5013852a9c6c
surinder singh <surinder83singh@gmail.com>
2016-02-22 22:09:14 +0530

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+var path 		= require('path');
+var ServeStatic = require('serve-static');
+var util 		= require('util');
+var _ 			= require('underscore');
+var fs 			= require('fs');
+
+function MDL(core, options){
+	var self = this;
+	options 		= options || {};
+	var combiner 	= options.httpCombiner || core.httpCombiner;
+
+	self.initHttp = function(app) {
+		var depsComponetsPath = path.join(__dirname,'/bower_components/');
+		var componetsPath = path.join(__dirname,'/http/mdl/');
+		var scriptsPath   = path.join(__dirname,'/http/scripts/');
+
+		if (combiner && combiner.addHttpFolders) {
+			combiner.addHttpFolders([
+				scriptsPath,
+				componetsPath,
+				depsComponetsPath
+			]);
+			combiner.addHttpFolderAlias({
+				deps: depsComponetsPath,
+				MDL:componetsPath,
+				'MDL/scripts': scriptsPath
+			})
+		};
+
+		var list = fs.readdirSync(componetsPath);
+		var ignoreList = ['.', '..', '.DS_Store'];
+		if (_.isArray(app.get('views'))) {
+			app.get('views').push(componetsPath);
+		}else{
+			app.set('views', [app.get('views'), componetsPath]);
+		}
+
+		_.each(list, function(file){
+			if (_.contains(ignoreList, file))
+				return;
+			// console.log("file", file)
+			app.get('/MDL/'+file, function(req, res, next) {
+	            res.render(file, {req: req});
+	        });
+		});
+
+		app.use('/deps', ServeStatic(depsComponetsPath));
+		app.use('/MDL/scripts', ServeStatic(scriptsPath));
+	    app.use('/MDL', ServeStatic(componetsPath));
+	}
+}
+
+module.exports = MDL;
